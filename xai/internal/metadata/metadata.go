da56ff52523eecb0a2864167d4152a5af80637f1
Stig-Ørjan Smelror <smelror@gmail.com>
2025-11-15 19:09:41 +0100

    chore: update version strings to 0.1.4

--- xai/internal/metadata/metadata.go
+++ xai/internal/metadata/metadata.go
@@ -51,7 +51,7 @@ type SDKMetadata struct {
 func NewSDKMetadata(apiKey string) *SDKMetadata {
 	return &SDKMetadata{
 		APIKey:        apiKey,
-		ClientVersion: "xai-sdk-go/0.1.2",
+		ClientVersion: "xai-sdk-go/0.1.4",
 		Environment:   "production",
 	}
 }
--- xai/internal/version/version.go
+++ xai/internal/version/version.go
@@ -8,7 +8,7 @@ import (
 )
 
 // SDKVersion is the current version of the xAI SDK Go client.
-const SDKVersion = "0.1.2"
+const SDKVersion = "0.1.4"
 
 // BuildInfo contains build and runtime information.
 type BuildInfo struct {
